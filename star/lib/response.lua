04c765b90328da67691925d9fa66265d5a6a0a49
Wanhe Han <wanhehan@WanhedeMacBook-Air.local>
2017-07-14 00:32:26 +0800

    variable name optimize
    variable name optimize

--- star/lib/response.lua
+++ star/lib/response.lua
@@ -1,10 +1,15 @@
+-- response.lua
+-- @modify 2017-07-14 00:10
+-- @author hanwanhe <hanwanhe@qq.com>
+-- @desc response instance
+
 local setmetatable = setmetatable
 local Response = {}
 local mt = {__index = Response}
 
 function Response:new()
   local instance = {
-    ngx_say = ngx.say
+    ngxSay = ngx.say
   }
   return setmetatable(instance, mt)
 end
